13c11e7aae1dc3ed5fd3174fa448e2c48116c099
julia <ju.pennas@gmail.com>
2023-10-16 21:22:57 -0300

    update migrations

---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,14 @@
+const createUsers = `
+
+create table if not exist users (
+	id integer primary key autoincrement, 
+    name varchar,
+    email varchar, 
+    password varchar, 
+    avatar varchar null,
+    create_at timestamp default current_timestamp
+    updated_at timestamp default current_timestamp
+)
+`;
+
+module.exports = createUsers;
